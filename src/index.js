fc01db42d1c9244b71aa0e9b7d88c845d7fa7567
Евгения Никонова <begale-dog@mail.ru>
2021-10-27 22:02:42 +0300

    Начата вёрстка

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+
+import "normalize.css";
+import "@fontsource/montserrat";
+import "roboto-fontface";
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+reportWebVitals();
